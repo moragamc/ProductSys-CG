445c908cd75a2d1c26770ba4b381da6b5c4350e2
Chris Moraga <christiangeo94@gmail.com>
2021-12-13 13:17:46 -0600

    Se agrega proyecto
    Se añade proyecto nuevo en c# MVC

---
+++ Proyecto2-GC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Proyecto2_GC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
